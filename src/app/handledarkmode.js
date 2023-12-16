c18414a5a956300824a33979e0a05a0dbab86dfd
Daniel A <DanielAAad01@gmail.com>
2023-12-15 18:12:05 -0800

    Added HandleDarkMode component to handle if page is in dark mode

---
+++ src/app/handledarkmode.js
@@ -0,0 +1,18 @@
+'use client';
+
+import { useState } from "react";
+import Header from "./header";
+
+export default function HandleDarkMode( {children} ) {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const handleSetDarkMode = () => {
+        setIsDarkMode(!isDarkMode)
+    }
+    
+    return (
+        <div className={(isDarkMode ? 'dark' : '') + " w-full overflow-x-hidden"}>
+            <Header handleSetDarkMode={handleSetDarkMode} />
+            { children } 
+        </div>
+     )
+}
