8fdedbdfb0bd0eca1e546109fd69af66b810a4e0
Daniel A <DanielAAad01@gmail.com>
2023-12-17 01:35:34 -0800

    User's dark mode preference is now stored in localStorage, allowing it to persist across page refreshes.

--- src/app/handledarkmode.js
+++ src/app/handledarkmode.js
@@ -1,14 +1,27 @@
 'use client';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./header";
 
 export default function HandleDarkMode( {children} ) {
+    let isDarkModeInstant = false;
     const [isDarkMode, setIsDarkMode] = useState(false);
     const handleSetDarkMode = () => {
-        setIsDarkMode(!isDarkMode)
+        isDarkModeInstant = !isDarkModeInstant
+        setIsDarkMode(isDarkModeInstant);
+        localStorage.setItem('darkModePrefs', JSON.stringify(isDarkModeInstant));
     }
     
+    useEffect(() => {
+        const retrievedDarkModePrefs = JSON.parse(localStorage.getItem('darkModePrefs'));
+        setIsDarkMode(retrievedDarkModePrefs);
+    }, [])
+
+    //persist selection of dark or light mode across page refresh. store isDarkMode in local storage
+    
+
+    
+
     return (
         <div className={(isDarkMode ? 'dark' : '') + " w-full overflow-x-hidden"}>
             <Header handleSetDarkMode={handleSetDarkMode} isDarkMode={isDarkMode} />
